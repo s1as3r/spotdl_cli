30363d1c69602b949658821d12ac8549fa9defbc
s1as3r <62838200+s1as3r@users.noreply.github.com>
2020-09-22 15:11:45 +0530

    Refactored code and added a bit more functionality

--- adv_spotdl_cli.py
+++ adv_spotdl_cli.py
@@ -1,10 +1,9 @@
+import os
 import sys
 from threading import Thread
-import os
-from os import system
 
-from spotdl.authorize.services import AuthorizeSpotify
 from spotdl import Spotdl, util
+from spotdl.authorize.services import AuthorizeSpotify
 from spotdl.helpers.spotify import SpotifyHelpers
 
 with open("spotify_keys.txt", "r+") as keys:
@@ -25,79 +24,84 @@ def logs():
 log = Thread(target=logs)
 
 
-def song():
+def song(link):
     log.start()
 
     def download():
         spotdl_instance.download_track(link)
+
     downloader = Thread(target=download)
     downloader.start()
 
 
-def album():
+def album(link):
     log.start()
     alb = helper_instance.fetch_album(link)
     helper_instance.write_album_tracks(alb, './album_tracks.txt')
 
     def download():
         spotdl_instance.download_tracks_from_file('album_tracks.txt')
+
     downloader = Thread(target=download)
     downloader.start()
 
 
-def playlist():
+def playlist(link):
     log.start()
     playlist = helper_instance.fetch_playlist(link)
     helper_instance.write_playlist_tracks(playlist, '.\playlist_tracks.txt')
 
     def download():
         spotdl_instance.download_tracks_from_file('playlist_tracks.txt')
+
     downloader = Thread(target=download)
     downloader.start()
 
 
-def textlist():
+def textlist(directory):
     log.start()
 
     def download():
-        spotdl_instance.download_tracks_from_file(link)
+        spotdl_instance.download_tracks_from_file(directory)
+
     downloader = Thread(target=download)
     downloader.start()
 
 
-def scan():
+def scan(link):
     if "spotify.com/track" in link.lower():
-        song()
+        song(link)
     elif "spotify.com/playlist" in link.lower():
-        playlist()
+        playlist(link)
     elif "spotify.com/album" in link.lower():
-        album()
+        album(link)
+    elif ".txt" in link.lower():
+        textlist(link)
     else:
-        song()
+        song(link)
 
 
 def select():
     global log_level
     global output_ext
 
-    system('cls')
-    # FIXME: Make this a litte less messy?
+    os.system('cls')
+
     print(
-        """
-    ==============================================================
-   |                                                               |
-   |   1) Log Level = {:17}2) Output Format = {:9}|
-   |   3) Set Manual Search String     4) Set Output Derictory     |
-   |                        5) Start                               |
-   |                                                               |
-    ==============================================================
-    """.format(log_level, output_ext)
+        f"""
+    =================================================================
+   |                                                                 |
+   |   1) Log Level = {log_level:19}2) Output Format = {output_ext:9}|
+   |   3) Set Manual Search String       4) Set Output Derictory     |
+   |   5) Start                          6) Exit                     |
+   |                                                                 |
+    =================================================================
+    """
     )
     return input("Enter option You want to change/set: ")
 
 
 def main():
-    global link
     global spotdl_instance
     global log_level
     global output_ext
@@ -108,37 +112,54 @@ def main():
     while again == 'Y':
         selection = select()
         if selection == '1':
-            system('cls')
+            os.system('cls')
             log_level = input('Log Level (INFO/DEBUG/ERROR/WARNING) = ')
             if log_level.lower() not in ['info', 'error', 'debug', 'warning']:
                 log_level = 'INFO'
 
         elif selection == '2':
-            system('cls')
+            os.system('cls')
             output_ext = input('Output Format (m4a,flac,mp3,opus,ogg) = ')
             if output_ext.lower() not in ['m4a', 'flac', 'mp3', 'opus', 'ogg']:
                 output_ext = 'mp3'
 
         elif selection == '3':
-            system('cls')
+            os.system('cls')
             print(f'Current Search String: {search_format}')
-            search_format = input('Enter Manual Search String: ')
+            search_format_inp = input('Enter Manual Search String: ')
+            if search_format_inp.lower() == 'default':
+                search_format = '{artist} - {track-name} lyrics'
+            elif not search_format_inp:
+                continue
+            else:
+                search_format = search_format_inp
 
         elif selection == '4':
-            system('cls')
+            os.system('cls')
             print(f'Current Output Directory: {output_file}')
-            output_file = input("Enter Path to the Directory: ")
+            output_file_inp = input("Enter Path to the Directory: ")
+            if output_file_inp.lower() == 'default':
+                output_file = os.getcwd()
+            elif not output_file_inp:
+                continue
+            else:
+                output_file = output_file_inp
 
         elif selection == '5':
             again = 'N'
+
+        elif selection == '6':
+            sys.exit()
+
+        else:
+            continue
 
     spotdl_instance = Spotdl(args={'output_ext': output_ext,
                                    'search_format': search_format,
                                    'output_file': output_file})
 
-    system('cls')
-    link = input("Enter the song/playlist/album link or path to a textlist: ")
-    scan()
+    os.system('cls')
+    scan(input("Enter the song/playlist/album link or path to a textlist: "))
 
 
 if __name__ == '__main__':
